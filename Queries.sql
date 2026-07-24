d2ad71307ac3392b343b5c41f347f75685598568
Shahad <ishahadalshaibani@gmail.com>
2026-07-24 21:55:21 +0300

    Add multiple UPDATE statements for flight records

--- Queries.sql
+++ Queries.sql
@@ -50,6 +50,36 @@ WHERE DEPARTURE_AIRPORT ='JED'
 
 
 
+UPDATE FLIGHT SET Departure_Airport = 'EAM', Arrival_Airport = 'LHR' WHERE Flight_ID = 1;
+
+UPDATE FLIGHT SET Departure_Airport = 'DMM', Arrival_Airport = 'AHB' WHERE Flight_ID = 2;
+
+UPDATE FLIGHT SET Departure_Airport = 'JED', Arrival_Airport = 'LHR' WHERE Flight_ID = 3;
+
+UPDATE FLIGHT SET Departure_Airport = 'RUH', Arrival_Airport = 'DOH' WHERE Flight_ID = 4;
+
+UPDATE FLIGHT SET Departure_Airport = 'DXB', Arrival_Airport = 'RUH' WHERE Flight_ID = 5;
+
+UPDATE FLIGHT SET Departure_Airport = 'AHB', Arrival_Airport = 'JED' WHERE Flight_ID = 6;
+
+UPDATE FLIGHT SET Departure_Airport = 'RUH', Arrival_Airport = 'DXB' WHERE Flight_ID = 7;
+
+UPDATE FLIGHT SET Departure_Airport = 'IST', Arrival_Airport = 'RUH' WHERE Flight_ID = 8;
+
+UPDATE FLIGHT SET Departure_Airport = 'TUU', Arrival_Airport = 'JED' WHERE Flight_ID = 9;
+
+UPDATE FLIGHT SET Departure_Airport = 'JED', Arrival_Airport = 'RUH' WHERE Flight_ID = 10;
+
+UPDATE FLIGHT SET Departure_Airport = 'JED', Arrival_Airport = 'OSL' WHERE Flight_ID = 11;
+
+UPDATE FLIGHT SET Departure_Airport = 'EAM', Arrival_Airport = 'JED' WHERE Flight_ID = 12;
+
+UPDATE FLIGHT SET Departure_Airport = 'RUH', Arrival_Airport = 'KEF' WHERE Flight_ID = 13;
+
+UPDATE FLIGHT SET Departure_Airport = 'RUH', Arrival_Airport = 'FCO' WHERE Flight_ID = 14;
+
+  
+
 SELECT F.FLIGHT_ID, COUNT(T.TICKET_ID) AS TOTAL_PASSENGER
 FROM ELIGHT F
 JOIN TICKET T ON F.FLIGHT_ID = T.FLIGHT_ID
