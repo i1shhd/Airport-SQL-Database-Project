088179ef0e4908c316433d29dcf1d24c7db43112
Shahad <ishahadalshaibani@gmail.com>
2026-07-24 23:23:44 +0300

    Add header comments to Trigger.sql
    Add header comments for Airport Database System.

--- Trigger.sql
+++ Trigger.sql
@@ -1,3 +1,9 @@
+-- ==========================================
+-- Airport Database System
+-- Oracle SQL
+-- Author: Shahad Alshaibani
+-- ==========================================
+
 CREATE OR REPLACE TRIGGER Calculate_AGE
 BEFORE INSERT OR UPDATE ON PASSENGER
 FOR EACH ROW
